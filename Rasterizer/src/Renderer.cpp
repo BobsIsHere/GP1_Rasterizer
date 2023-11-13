840a913796370d7e180cb2258c3398e3d2710054
BobsIsHere <maritte.kindt@student.howest.be>
2023-11-13 21:42:16 +0100

    W6 Lab - Todo 4

--- Rasterizer/src/Renderer.cpp
+++ Rasterizer/src/Renderer.cpp
@@ -24,12 +24,12 @@ Renderer::Renderer(SDL_Window* pWindow) :
 	m_pBackBuffer = SDL_CreateRGBSurface(0, m_Width, m_Height, 32, 0, 0, 0, 0);
 	m_pBackBufferPixels = (uint32_t*)m_pBackBuffer->pixels;
 
-	//m_pDepthBufferPixels = new float[m_Width * m_Height];
+	m_pDepthBufferPixels = new float[m_Width * m_Height];
 }
 
 Renderer::~Renderer()
 {
-	//delete[] m_pDepthBufferPixels;
+	delete[] m_pDepthBufferPixels;
 }
 
 void Renderer::Update(Timer* pTimer)
@@ -45,8 +45,8 @@ void Renderer::Render()
 
 	//Render_W1_Part1();		//rasterizer stage only
 	//Render_W1_Part2();		//projection stage (camera)
-	Render_W1_Part3();		//barycentric coordinates
-	//Render_W1_Part4();		//depth buffer
+	//Render_W1_Part3();		//barycentric coordinates
+	Render_W1_Part4();		//depth buffer
 	//Render_W1_Part5();		//boundingbox optimization
 
 	//RENDER LOGIC
@@ -244,7 +244,87 @@ void Renderer::Render_W1_Part3()
 
 void Renderer::Render_W1_Part4()
 {
+	//define triangle - vertices in WOLRD space
+	bool isIntriangle{ false };
+	std::fill_n(m_pDepthBufferPixels, m_Width * m_Height, FLT_MAX);
 
+	std::vector<Vertex> vertices_world
+	{
+		//Triangle 0 
+		{{0.f, 2.f, 0.f}, {1, 0, 0}},
+		{{1.5f, -1.f, 0.f}, {1, 0, 0}},
+		{{-1.5f, -1.f, 0.f}, {1, 0, 0}},
+		//Triangle 1
+		{{0.f, 4.f, 2.f}, {1, 0, 0}},
+		{{3.f, -2.f, 2.f}, {0, 1, 0}},
+		{{-3.f, -2.f, 2.f}, {0, 0, 1}},
+	};
+
+	std::vector<Vertex> vertices_transformed{};
+	vertices_transformed.reserve(vertices_world.size());
+
+	VertexTransformationFunction(vertices_world, vertices_transformed);
+
+	//clear back buffer
+	SDL_FillRect(m_pBackBuffer, &m_pBackBuffer->clip_rect, SDL_MapRGB(m_pBackBuffer->format, 100, 100, 100));
+
+	//go over triangle, per 3 vertices
+	for (int triangleIdx{}; triangleIdx < vertices_transformed.size(); triangleIdx += 3)
+	{
+		//go over each pixel is in screen space
+		for (int px{}; px < m_Width; ++px)
+		{
+			for (int py{}; py < m_Height; ++py)
+			{
+				//define current pixel in screen space
+				const Vector3 P{ px + 0.5f, py + 0.5f, 0.f };
+				ColorRGB finalColour{ 0.f, 0.f, 0.f };
+
+				if (Calculate2DCrossProduct(vertices_transformed[triangleIdx + 2].position, vertices_transformed[triangleIdx + 1].position, P) < 0.f ||
+					Calculate2DCrossProduct(vertices_transformed[triangleIdx + 1].position, vertices_transformed[triangleIdx + 0].position, P) < 0.f ||
+					Calculate2DCrossProduct(vertices_transformed[triangleIdx + 0].position, vertices_transformed[triangleIdx + 2].position, P) < 0.f)
+				{
+					isIntriangle = false;
+				}
+				else
+				{
+					isIntriangle = true;
+				}
+
+				if (isIntriangle)
+				{
+					const float w0{ Calculate2DCrossProduct(vertices_transformed[triangleIdx + 1].position, vertices_transformed[triangleIdx + 2].position, P) };
+					const float w1{ Calculate2DCrossProduct(vertices_transformed[triangleIdx + 2].position, vertices_transformed[triangleIdx + 0].position, P) };
+					const float w2{ Calculate2DCrossProduct(vertices_transformed[triangleIdx + 0].position, vertices_transformed[triangleIdx + 1].position, P) };
+					const float triangleArea{ Calculate2DCrossProduct(vertices_transformed[triangleIdx + 0].position, 
+																   vertices_transformed[triangleIdx + 1].position, 
+																      vertices_transformed[triangleIdx + 2].position) };
+					
+					//interpolate depth value
+					const int bufferIdx{ px + (py * m_Width) };
+					const float interpolateDepth{ 1.f / (w0 * vertices_transformed[triangleIdx + 0].position.z + 
+												  w1 * vertices_transformed[triangleIdx + 1].position.z +
+												  w2 * vertices_transformed[triangleIdx + 2].position.z) };
+					
+					if (interpolateDepth < m_pDepthBufferPixels[bufferIdx])
+					{
+						m_pDepthBufferPixels[bufferIdx] = interpolateDepth;
+
+						finalColour = { vertices_transformed[triangleIdx + 0].color * (w0 / triangleArea) +
+									    vertices_transformed[triangleIdx + 1].color * (w1 / triangleArea) +
+									    vertices_transformed[triangleIdx + 2].color * (w2 / triangleArea) };
+
+						finalColour.MaxToOne();
+
+						m_pBackBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBackBuffer->format,
+							static_cast<uint8_t>(finalColour.r * 255),
+							static_cast<uint8_t>(finalColour.g * 255),
+							static_cast<uint8_t>(finalColour.b * 255));
+					}
+				}
+			}
+		}
+	}
 }
 
 void Renderer::Render_W1_Part5()
--- Rasterizer/src/Renderer.h
+++ Rasterizer/src/Renderer.h
@@ -30,17 +30,18 @@ namespace dae
 		void Update(Timer* pTimer);
 		void Render();
 
+		bool SaveBufferToImage() const;
+
+		//------ Render Functions ------
 		void Render_W1_Part1();
 		void Render_W1_Part2();
 		void Render_W1_Part3();
 		void Render_W1_Part4();
 		void Render_W1_Part5();
 
+		//------ Own Functions ------
 		float Calculate2DCrossProduct(const Vector3& a, const Vector3& b, const Vector3& c);
 		float Calculate2DCrossProduct(const Vector2& a, const Vector2& b, const Vector2& c);
-
-		bool SaveBufferToImage() const;
-
 		void VertexTransformationFunction(const std::vector<Vertex>& vertices_in, std::vector<Vertex>& vertices_out) const;
 
 	private:
@@ -50,7 +51,7 @@ namespace dae
 		SDL_Surface* m_pBackBuffer{ nullptr };
 		uint32_t* m_pBackBufferPixels{};
 
-		//float* m_pDepthBufferPixels{};
+		float* m_pDepthBufferPixels{};
 
 		Camera m_Camera{};
 
